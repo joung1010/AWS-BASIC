8cb70f81a2cf5a222cdb4ba91b1ca505b6693587
mason park <joung4342@gmail.com>
2025-04-03 21:23:47 +0900

    feat(aws): basic health check

---
+++ src/main/java/com/business/dream/endpoint/common/CommonController.java
@@ -0,0 +1,25 @@
+package com.business.dream.endpoint.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <b> CommonController </b>
+ *
+ * @author jh.park
+ * @version 0.1.0
+ * @since 2025-04-03
+ */
+
+@Controller
+@RequestMapping("/api/common")
+public class CommonController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "UP";
+    }
+
+
+}
