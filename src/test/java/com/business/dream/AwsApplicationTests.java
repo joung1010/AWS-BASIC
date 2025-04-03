0e7b60d06b13a9872ee23809505d728cbef269ad
mason park <joung4342@gmail.com>
2025-04-03 21:13:55 +0900

    feat(aws): git init

---
+++ src/test/java/com/business/dream/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
